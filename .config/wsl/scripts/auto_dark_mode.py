9460a4e6009d294f4bf673a0d960278913020666
Si Yong Kim <zzandland@gmail.com>
2021-02-18 16:07:39 -0800

    Change font and add bat config for ripgrip preview colorscheme

--- .config/wsl/scripts/auto_dark_mode.py
+++ .config/wsl/scripts/auto_dark_mode.py
@@ -35,6 +35,7 @@ def is_dark_theme() -> bool:
 
 def update_zshrc(new_dark_mode: bool):
     ZSHRC_PATH = '~/.zshrc'
+    BATCONFIG_PATH = '~/.config/bat/config'
 
     with open(os.path.expanduser(ZSHRC_PATH), "r") as zshrc:
         lines = zshrc.readlines()
@@ -45,6 +46,10 @@ def update_zshrc(new_dark_mode: bool):
             print('Updating Neovim background color')
             lines[-1] = 'export NVIM_BACKGROUND=%s\n' % (new_color)
             open(os.path.expanduser(ZSHRC_PATH), 'w').writelines(lines)
+
+            with open(os.path.expanduser(BATCONFIG_PATH), 'w') as batconfig:
+                theme = 'gruvbox' if new_dark_mode else 'gruvbox-light'
+                batconfig.write('--theme="%s"' % (theme))
 
 def update_config(config_path: str, data: Dict):
     with open(config_path, 'w') as config:
